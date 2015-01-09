d50b70fbf620b9953fee2b04a65452186d0949e3
Erik Carlsson <info@code301.com>
2015-01-09 22:59:30 +0100

    Done

---
+++ lib/Map/Metro/Plugin/Map/Lille.pm
@@ -0,0 +1,53 @@
+use 5.14.0;
+
+package Map::Metro::Plugin::Map::Lille;
+
+# VERSION
+# ABSTRACT: Map::Metro map for Lille
+
+use Moose;
+with 'Map::Metro::Plugin::Map';
+
+has '+mapfile' => (
+    default => 'map-lille.metro',
+);
+sub map_version {
+    return $VERSION;
+}
+sub map_package {
+    return __PACKAGE__;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 SYNOPSIS
+
+    use Map::Metro;
+    my $graph = Map::Metro->new('Lille')->parse;
+
+Or
+
+	$ map-metro.pl route Lille 1 4 
+
+=head1 DESCRIPTION
+
+See L<Map::Metro> for usage information.
+
+=head1 Status
+
+L<Map::Metro::Plugin::Map::Lille::Lines>
+
+This map includes:
+
+=for :list
+* The two metro lines 
+
+=for HTML <p><a href="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Lille/master/static/images/lille.png"><img src="https://raw.githubusercontent.com/Csson/p5-Map-Metro-Lille/master/static/images/lille.png" style="max-width: 600px" /></a></p>
+
+=cut
